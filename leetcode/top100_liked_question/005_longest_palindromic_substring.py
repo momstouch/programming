53feba6c85b04955a45e64a8fddf66f74ea40dea
momstouch <yo9rtace@gmail.com>
2020-01-25 02:47:42 +0900

    leetcode top 100 liked questions series 005_longest_palindromic_substring.py the code has been accepted but it is too slow. it has to be modified.

--- leetcode/top100_liked_question/005_longest_palindromic_substring.py
+++ leetcode/top100_liked_question/005_longest_palindromic_substring.py
@@ -2,8 +2,28 @@
 
 
 class Solution:
+    # Runtime: 524 ms
+    # Memory Usage: 12.8 MB
+    def f(self, s: str, left: int, right: int):
+        if left >= 0 and right < len(s):
+            if s[left] == s[right]:
+                if self.max_len < right - left + 1:
+                    self.max_len = right - left + 1
+                    self.result = [left, right]
+                self.f(s, left - 1, right + 1)
+
+
     def longestPalindrome(self, s: str) -> str:
-        
+        if len(s) < 2 or s == s[::-1]:
+            return s
+
+        self.max_len = 1
+        self.result = [0, 0]
+        for i in range(len(s)):
+            self.f(s, i, i + 1)
+            self.f(s, i, i + 2)
+
+        return s[self.result[0]: self.result[1] + 1]
 
 
 testcase = [
