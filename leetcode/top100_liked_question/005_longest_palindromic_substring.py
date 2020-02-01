6a7bd84058f8a045066231d91eeea376dae509c5
momstouch <yo9rtace@gmail.com>
2020-02-01 21:59:15 +0900

    leetcode top100 liked questions series 005. longest palindromic substring

--- leetcode/top100_liked_question/005_longest_palindromic_substring.py
+++ leetcode/top100_liked_question/005_longest_palindromic_substring.py
@@ -26,6 +26,29 @@ class Solution:
         return s[self.result[0]: self.result[1] + 1]
 
 
+    def longestPalindrome2(self, s: str) -> str:
+        str_len = len(s)
+        # if length of string < 2 or s is palindrome already
+        if str_len < 2 or s == s[::-1]:
+            return s
+
+        start, max_len = 0, 1
+
+        for i in range(1, str_len):
+            odd_start = i - max_len - 1
+            even_start = i - max_len
+            odd = s[odd_start:i + 1]  # len(odd) = max_len + 2
+            even = s[even_start:i + 1]  # len(even) = max_len + 1
+
+            if odd_start >= 0 and odd == odd[::-1]:
+                start = odd_start
+                max_len += 2
+            elif even_start >= 0 and even == even[::-1]:
+                start = even_start
+                max_len += 1
+        return s[start:start + max_len]
+
+
 testcase = [
         "babad",        # "bab" or "aba"
         "cbbd",         # "bb"
@@ -33,4 +56,5 @@ testcase = [
 
 s = Solution()
 for t in testcase:
-    print(s.longestPalindrome(t))
+    #print(s.longestPalindrome(t))
+    print(s.longestPalindrome2(t))
