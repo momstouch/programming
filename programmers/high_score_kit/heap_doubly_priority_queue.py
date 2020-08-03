3ca9b09de286fbdf7c16fed9c89cc4c4ad0cc06f
momstouch <yo9rtace@gmail.com>
2020-08-04 00:44:52 +0900

    complete the solution

--- programmers/high_score_kit/heap_doubly_priority_queue.py
+++ programmers/high_score_kit/heap_doubly_priority_queue.py
@@ -1,11 +1,12 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42628
 import heapq
+import time
 
 
 def solution(operations):
     minq = []
     maxq = []
-    deleted = []
+    table = {}
 
     for idx, opstring in enumerate(operations):
         op, num = opstring.split(' ')
@@ -14,34 +15,64 @@ def solution(operations):
         if op == "I":
             heapq.heappush(minq, (num, idx))
             heapq.heappush(maxq, (-num, idx))
-        else:
+            table[idx] = True
+        elif table:
             if num > 0: # pop max
                 n, i = heapq.heappop(maxq)
+                n = -n
             else:
                 n, i = heapq.heappop(minq)
 
-            deleted.append(i)
+            del table[i]
 
     answer = [0, 0]
 
     if minq and maxq:
-        while True:
+        while maxq:
             n, i = heapq.heappop(maxq)
-            if i not in deleted:
+            if table.get(i, False):
                 answer[0] = -n
                 break
-        while True:
+        while minq:
             n, i = heapq.heappop(minq)
-            if i not in deleted:
+            if table.get(i, False):
                 answer[1] = n
                 break
 
     return answer
 
 
+def better_solution(operations):
+    heap = []
+
+    for operation in operations:
+        op, num = operation.split(' ')
+        num = int(num)
+
+        if op == "I":
+            heapq.heappush(heap, num)
+        elif heap:
+            if num < 0:
+                heapq.heappop(heap)
+            else:
+                heap.remove(max(heap))
+
+    if not heap:
+        return [0, 0]
+
+    return [max(heap), heap[0]]
+
+
 cases = [
         ["I 16","D 1"],              # 	[0,0]
         ["I 7","I 5","I -5","D -1"], #	[7,5]
         ]
+t0 = time.time()
 for case in cases:
     print(solution(case))
+print("%.6f" % (time.time() - t0))
+
+t0 = time.time()
+for case in cases:
+    print(better_solution(case))
+print("%.6f" % (time.time() - t0))
