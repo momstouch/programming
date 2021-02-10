661d0e6b6a779dd33dacde00daee4eb88218fc03
momstouch <yo9rtace@gmail.com>
2021-02-11 00:40:02 +0900

    greedy_joystick.py

--- programmers/high_score_kit/greedy_joystick.py
+++ programmers/high_score_kit/greedy_joystick.py
@@ -1,37 +1,54 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42860
 
+def next_pos_and_weight(i, name):
+    j = i
+    while j < len(name) and name[j] == 'A':
+        j += 1
+    jcnt = j - i
+    kcnt = 0
+
+    k = i
+    while len(name) + k if k < 0 else k != i + 1:
+        if name[len(name) + k if k < 0 else k] != 'A':
+            break
+        kcnt += 1
+        k -= 1
+
+    if j >= len(name) or (name[j] == 'A' and name[k] == 'A'):
+        return None, None
+    if jcnt <= kcnt:
+        return j, jcnt
+    else:
+        return k, kcnt
+
+
 def solution(name):
-    asciis = [ord(c) - ord('A') for c in name]
-    target = len(name)
-
-    n_alpha = ord('Z') - ord('A') + 1
-    center = n_alpha // 2
-
+    i = 0 # current pos over name
+    lname = list(name)
     ans = 0
-    last_pos = 0
-    for i, num in enumerate(asciis):
-        if num: # if alphabet is not an A
-            if num > center:
-                ans += n_alpha - num
-            else:
-                ans += num
 
-            back = min(i, last_pos) - max(i, last_pos) + target
-            front = abs(i - last_pos)
+    while True:
+        nexti, step = next_pos_and_weight(i, lname)
+        if nexti is None:
+            break
 
-            ans += min(back, front)
+        #print(nexti, step, lname[nexti])
+        ans += min(ord(lname[nexti]) - ord('A'), ord('Z') - ord(lname[nexti]) + 1)
+        ans += step
 
-            last_pos = i
+        lname[nexti] = 'A'
+        i = nexti
 
+    #print(ans)
     return ans
 
 
 cases = [
-        "JEROEN",       # 56
-        "JAN",          # 23
-        "BABBAABA",     # 9
-        "BBBBAABB",     # 9
+        ("JEROEN", 56),
+        ("JAN", 23),
+        ("BABBAABA", 9),
+        ("BBBBAABB", 9),
         ]
 
-for case in cases:
-    print(solution(case))
+for name, answer in cases:
+    assert solution(name) == answer
