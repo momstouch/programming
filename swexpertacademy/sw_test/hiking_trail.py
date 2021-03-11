fe5954eb9b7a262f04e409b3617f74e6d16a7e83
momstouch <yo9rtace@gmail.com>
2021-03-11 23:58:17 +0900

    solved hiking_trail

--- swexpertacademy/sw_test/hiking_trail.py
+++ swexpertacademy/sw_test/hiking_trail.py
@@ -1,25 +1,54 @@
 # https://swexpertacademy.com/main/solvingProblem/solvingProblem.do
 
+import copy
+
 DX = [-1,0,1,0]
 DY = [0,-1,0,1]
 
-def dfs(i, j, depth):
+def dfs(i, j, depth, dig, ans):
+    go_deeper = True
+    for di, dj in zip(DX, DY):
+        ni = i + di
+        nj = j + dj
+
+        if ni >= 0 and ni < N and nj >= 0 and nj < N and\
+                not visit[ni][nj]:
+            if graph[i][j] > graph[ni][nj]:
+                visit[ni][nj] = True
+                dfs(ni, nj, depth + 1, dig, ans)
+                visit[ni][nj] = False
+            elif graph[i][j] <= graph[ni][nj] and not dig and \
+                    graph[ni][nj] - K < graph[i][j]:
+                graph[ni][nj] = graph[i][j] - 1
+                visit[ni][nj] = True
+                dfs(ni, nj, depth + 1, True, ans)
+                graph[ni][nj] = origin[ni][nj]
+                visit[ni][nj] = False
+            else:
+                go_deeper = False
+
+    if not go_deeper:
+        ans[0] = max(ans[0], depth)
+
 
 T = int(input())
 for case in range(1, T + 1):
     N, K = list(map(int, input().split(' ')))
-    trail = [list(map(int, input().split(' '))) for _ in range(N)]
+    graph = [list(map(int, input().split(' '))) for _ in range(N)]
+    origin = copy.deepcopy(graph)
     visit = [[False for _ in range(N)] for _ in range(N)]
 
     _max = 0
-    for row in trail:
+    for row in graph:
         m = max(row)
         _max = max(m, _max)
 
-    ans = 0
+    ans = [0]
     for i in range(N):
         for j in range(N):
-            if trail[i][j] == _max:
-                ans = max(ans, dfs(i, j, 0))
+            if graph[i][j] == _max:
+                visit[i][j] = True
+                dfs(i, j, 1, False, ans)
+                visit[i][j] = False
 
-    print("#%d" % (case), ans)
+    print("#%d" % (case), ans[0])
